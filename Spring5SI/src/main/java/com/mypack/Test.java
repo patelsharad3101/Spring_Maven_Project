41d6becda4318cc8aba5cc5e883af7128e41d8eb
Sharad Patel <patelsharad3101@gmail.com>
2018-11-15 14:36:26 +0530

    First Commit

---
+++ Spring5SI/src/main/java/com/mypack/Test.java
@@ -0,0 +1,17 @@
+package com.mypack;
+
+import org.springframework.beans.factory.BeanFactory;  
+import org.springframework.beans.factory.xml.XmlBeanFactory;  
+import org.springframework.core.io.*;  
+  
+public class Test {  
+    public static void main(String[] args) {  
+          
+        Resource r=new ClassPathResource("spring.xml");  
+        BeanFactory factory=new XmlBeanFactory(r);  
+          
+        Employee e=(Employee)factory.getBean("obj");  
+        e.display();  
+          
+    }  
+}  
